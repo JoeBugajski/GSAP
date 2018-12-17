d3ceccf6711d222f9ca8dd4c6fdda6aa721131f0
Joe Bugajski <yogurtmystery@gmail.com>
2018-12-17 01:17:14 -0600

    -skewX ease -.from ease

--- module 4/js/app.js
+++ module 4/js/app.js
@@ -1,4 +1,4 @@
-$(document).ready(function(){
+$(window).load(function(){
     init();
 });
 
@@ -8,14 +8,21 @@ function init(){
     // normal TimelineMax timeline
     var toolTimeline = new TimelineMax(); 
     //duration of animation
-    var duration = .5;  
+    var duration = .6;  
     // Here we see a simplified syntax 
     // for adding a Tween that is availabe 
     // with TweenMax
-    toolTimeline.to('#toolBox', duration, {y: 600, ease:Linear.easeInOut}); 
-    toolTimeline.to('#weightLifter', duration, {y: 600, ease:Bounce.easeInOut});
-    toolTimeline.to('#crazy', duration, {y: 600, ease:Elastic.easeInOut});
-    toolTimeline.to('#leaf', duration, {y: 600, ease:Back.easeInOut});
+    toolTimeline.from('.mainTitle', duration, {scale:25, ease:Linear.easeInOut}); 
+   
+    toolTimeline.from('.title', duration, {opacity: 0, scale: 25, ease:Linear.easeInOut}, .2);
+  
+    toolTimeline.to('.superman img', duration, {left: 0, ease:Back.easeOut}); 
+    toolTimeline.from('.superman img', duration, {skewX: '20deg', ease:Back.easeOut}, .9); 
+
+    toolTimeline.to('.mainTitle', duration, {right: -90, ease:Back.easeInOut}, .7);
+
+    toolTimeline.staggerTo('.tools', duration, {top: 650, ease:Back.easeInOut}, 1.2, 1.7)
+
     // TweenLite syntax: 
     // toolTimeline.add(TweenLite.to('#toolBox', duration, {y:-100, ease:Linear.easeInOut}));
 }
