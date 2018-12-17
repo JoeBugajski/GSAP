b009c823efe1ccd68000b7466cc8509498aeaf6c
Joe Bugajski <yogurtmystery@gmail.com>
2018-12-16 18:57:07 -0600

    Tween function with no jQuery

--- module 4/js/app.js
+++ module 4/js/app.js
@@ -2,13 +2,20 @@ $(document).ready(function(){
     init();
 });
 
+// This function doesn't use jquery syntax! 
 
 function init(){
-    var toolTimeline = new TimelineLite();
-    var duration = .5;
-    toolTimeline.to('#toolBox', duration, {y: 600, ease:Linear.easeInOut});
+    // normal TimelineMax timeline
+    var toolTimeline = new TimelineMax(); 
+    //duration of animation
+    var duration = .5;  
+    // Here we see a simplified syntax 
+    // for adding a Tween that is availabe 
+    // with TweenMax
+    toolTimeline.to('#toolBox', duration, {y: 600, ease:Linear.easeInOut}); 
     toolTimeline.to('#weightLifter', duration, {y: 600, ease:Bounce.easeInOut});
     toolTimeline.to('#crazy', duration, {y: 600, ease:Elastic.easeInOut});
     toolTimeline.to('#leaf', duration, {y: 600, ease:Back.easeInOut});
-
+    // TweenLite syntax: 
+    // toolTimeline.add(TweenLite.to('#toolBox', duration, {y:-100, ease:Linear.easeInOut}));
 }
