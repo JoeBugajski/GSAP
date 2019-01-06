a05ea032c2553ed72d3e25dc3ea6c5edfc5c2956
Joe Bugajski <yogurtmystery@gmail.com>
2019-01-05 21:35:37 -0600

    animated head

--- module 4/js/app.js
+++ module 4/js/app.js
@@ -8,20 +8,13 @@ function init(){
     // normal TimelineMax timeline
     var toolTimeline = new TimelineMax(); 
     //duration of animation
-    var duration = .6;  
+    var duration = 1;  
     // Here we see a simplified syntax 
     // for adding a Tween that is availabe 
     // with TweenMax
-    toolTimeline.from('.mainTitle', duration, {scale:25, ease:Linear.easeInOut}); 
-   
-    toolTimeline.from('.title', duration, {opacity: 0, scale: 25, ease:Linear.easeInOut}, .2);
-  
-    toolTimeline.to('.superman img', duration, {scale: 1.5, left: 0, ease:Back.easeOut}); 
-    toolTimeline.from('.superman img', duration, {scale: .5, skewX: '20deg', ease:Back.easeOut}, .9); 
-
-    toolTimeline.to('.mainTitle', duration, {right: -90, ease:Back.easeInOut}, .7);
-
-    toolTimeline.staggerTo('.tools', duration, {top: 30, ease:Back.easeInOut}, 1.2, 1.7)
+    
+    toolTimeline.from('.superman img', duration, { scale: 1, left: 0, rotationY: 2160, ease:Back.ease}); 
+    toolTimeline.from('.superman img', duration, { scale: .5, rotationX: 2160, ease:Back.ease}, .8); 
 
     // TweenLite syntax: 
     // toolTimeline.add(TweenLite.to('#toolBox', duration, {y:-100, ease:Linear.easeInOut}));
