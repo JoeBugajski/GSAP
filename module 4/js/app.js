05d6af723359b773d4155ee73be689910c89b6ec
Joe Bugajski <yogurtmystery@gmail.com>
2019-01-07 23:20:11 -0600

    simplified floating head animation

--- module 4/js/app.js
+++ module 4/js/app.js
@@ -8,13 +8,13 @@ function init(){
     // normal TimelineMax timeline
     var toolTimeline = new TimelineMax(); 
     //duration of animation
-    var duration = 1;  
+    var duration = 2;  
     // Here we see a simplified syntax 
     // for adding a Tween that is availabe 
     // with TweenMax
     
-    toolTimeline.from('.superman img', duration, { scale: 1, left: 0, rotationY: 2160, ease:Back.ease}); 
-    toolTimeline.from('.superman img', duration, { scale: .5, rotationX: 2160, ease:Back.ease}, .8); 
+    toolTimeline.to('.superman img', duration, { left: 0, rotationY: 180, ease:Back.ease}); 
+    toolTimeline.from('.superman img', duration, { rotationX: 180, ease:Back.ease}, .8); 
 
     // TweenLite syntax: 
     // toolTimeline.add(TweenLite.to('#toolBox', duration, {y:-100, ease:Linear.easeInOut}));
